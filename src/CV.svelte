2ee06ef33653e3957773ef2b3e4de9f0a99496c3
sanzhar.shakarim <sanzhar.shakarim@wabtec.com>
2024-08-08 09:31:11 -0400

    CV page finished

--- src/CV.svelte
+++ src/CV.svelte
@@ -6,28 +6,126 @@
 
 <main class="px-8">
   <div class="rounded-lg bg-[rgba(0,0,0,0.2)] p-10 mt-10 text-gray-100">
-    <div class="flex justify-between items-start">
+    <div class="flex justify-center mb-10">
       <h1 class="text-2xl font-bold">Resume</h1>
     </div>
     
-
-    <table class="w-full mt-4 text-xs">
-      <h2 class="px-2 py-1 text-xs font-bold">EDUCATION</h2>
-      <tr>
-        <td class="px-2 py-1 text-xs">Carnegie Mellon University <br>
-          Master of Engineering and Technology Innovation Management<br>
-          GPA: 3.67/4.00</td>
-        <td class="px-2 py-1 text-xs">Pittsburgh, PA <br>
-          12/2024<br></td>
-      </tr>
-      <tr>
-        <td class="px-2 py-1 text-xs">Nazarbayev University<br>
-          Bachelor of Electrical and Computer Engineering<br>
-          GPA:3.39/4.00</td>
-        <td class="px-2 py-1 text-xs">Astana, Kazakhstan<br>
-          May 2023</td>
-      </tr>
-    </table>
+    <div class="flex justify-center">
+      <table class="w-3/5 mt-4 text-xs">
+        <thead>
+          <tr>
+            <th class="px-2 py-2 text-xs font-bold border-t text-left" colspan="2">EDUCATION</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="px-2 text-xs">
+              <p class="font-bold">Carnegie Mellon University</p>
+              Master of Engineering and Technology Innovation Management<br>
+              GPA: 3.67/4.00
+            </td>
+            <td class="px-2 text-xs">Pittsburgh, PA <br>
+              12/2024<br></td>
+          </tr>
+          <tr>
+            <td class="px-2 py-2 text-xs">
+              <p class="font-bold">Nazarbayev University</p>
+              Bachelor of Electrical and Computer Engineering<br>
+              GPA: 3.39/4.00
+            </td>
+            <td class="px-2 text-xs">Astana, Kazakhstan<br>
+              05/2023</td>
+          </tr>
+        </tbody>
+        <thead>
+          <tr>
+            <th class="px-2 py-2 text-xs font-bold border-t text-left" colspan="2">EXPERIENCE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="px-2 text-xs">
+              <p class="font-bold">Wabtec</p>
+              Software Engineering Intern
+              <ul class="list-disc pl-6">
+                <li>Led a project to upgrade Matlab/Simulink to the latest version, implemented a new DDS Blockset, and validated compatibility with existing software models and code</li>
+                <li>Evaluated and compared two requirement management tools for a company-wide deployment to manage over 10,500 Simulink requirements</li>
+              </ul>
+            </td>
+            <td class="px-2 py-2 text-xs">Erie, PA <br>
+              05/2024 – 08/2024<br></td>
+          </tr>
+          <tr>
+            <td class="px-2 py-2 text-xs">
+              <p class="font-bold">Nazarbayev University</p>
+              Research Assistant
+              <ul class="list-disc pl-6">
+                <li>Led a project team that published an IEEE paper (as first author) on a microfluidic device sensor integration, as part of a larger 3-year, $100,000 government-funded project</li>
+                <li>Focused on developing innovative designs to grow various cell lines for the organ-on-a-chip device with integrated oxygen and CO2 sensors</li>
+              </ul>
+            </td>
+            <td class="px-2 py-2 text-xs">Astana, Kazakhstan<br>
+              09/2021 – 11/2023</td>
+          </tr>
+          <tr>
+            <td class="px-2 pb-2 text-xs">
+              <p class="font-bold">Samruk Energo – Digital Transformation Department</p>
+              Electrical Engineering Intern
+              <ul class="list-disc pl-6">
+                <li>Conducted vibration analysis of different blocks in one of the largest power plants in Kazakhstan, with a total installed capacity of 4,000 MW</li>
+                <li>Presented findings to senior management for deployment</li>
+              </ul>
+            </td>
+            <td class="px-2 py-2 text-xs">Astana, Kazakhstan<br>
+              11/2021 – 01/2022</td>
+          </tr>
+        </tbody>
+        <thead>
+          <tr>
+            <th class="px-2 py-2 text-xs font-bold border-t text-left" colspan="2">ARTICLE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="px-2 pb-2 text-xs" colspan="2">
+              S. Shakarim, D. Tosi and G. Kulsharova, "Integration of fiber optic sensors in organ-on-a-chip devices towards label-free cell viability assays," 2022 IEEE Sensors Applications Symposium (SAS), Sundsvall, Sweden, 2022, pp. 1-6, doi: 10.1109/SAS54819.2022.9881355.
+            </td>
+          </tr>
+        </tbody>
+        <thead>
+          <tr>
+            <th class="px-2 py-2 text-xs font-bold border-t text-left" colspan="2">ADDITIONAL EXPERIENCE</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="px-2 text-xs">
+              <p class="font-bold">Shkolkovo educational portal for the exam preparation</p>
+              Senior Tutor
+              <ul class="list-disc pl-6">
+                <li>Conducted live video lessons on an  
+                  <a href="https://www.youtube.com/channel/UC8wRlAEVrYf1ABTtP4dk5TQ" class="underline hover:bg-[rgba(0,0,0,0.2)]"> online platform</a>
+                </li>
+                <li>90% of students were admitted to top universities as a result of my lessons</li>
+              </ul>
+            </td>
+            <td class="px-2 py-2 text-xs">Moscow, Russia <br>
+              08/2020 – 05/2021<br></td>
+          </tr>
+          <tr>
+            <td class="px-2 py-2 text-xs">
+              <p class="font-bold py-2">VOLUNTEER WORK</p>
+              <ul class="list-disc pl-6">
+                <li>Involved in organizations of Harvard College Conference in Astana, Kazakhstan [HPAIR 2019]</li>
+                <li>Managed several concerts of pop artists</li>
+              </ul>
+            </td>
+            <td class="px-2 py-2 text-xs">Astana, Kazakhstan <br>
+              08/2019 – 11/2019<br></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
   
 </main>
